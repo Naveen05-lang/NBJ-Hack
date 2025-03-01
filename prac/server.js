aa20b753dcece66a58ec7ea860eaa76fcc34a32a
Naveen05-lang <naveenthalari2000@gmail.com>
2025-03-01 17:08:36 +0530

    added

--- prac/server.js
+++ prac/server.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 const connectDB=require("./config/db");
 const authRoutes = require("./routes/auth.route");
 const yogaRoutes = require("./routes/yoga.route");
-// const ratingRoutes = require("./routes/ratingRoutes");
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 connectDB();
 app.use("/auth", authRoutes);
