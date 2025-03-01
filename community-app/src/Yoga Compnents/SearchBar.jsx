1f7646aa8210540856adddb94dbaf744d666e9f7
unknown <janhavi.walde22@gmail.com>
2025-03-01 14:44:08 +0530

    yoga-dash

---
+++ community-app/src/Yoga Compnents/SearchBar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearch}
+        placeholder="Search Yoga Asanas"
+        className="form-control"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
